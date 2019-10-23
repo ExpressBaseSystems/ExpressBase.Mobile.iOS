29142346698f2fbd74211da37556cbc254f6d3b1
Amalkdev <amaldev@expressbase.com>
2019-10-23 22:36:16 +0530

    c

---
+++ Helpers/NativeHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using ExpressBase.Mobile.iOS.Helpers;
+using ExpressBase.Mobile.Models;
+using Foundation;
+using UIKit;
+
+[assembly: Xamarin.Forms.Dependency(typeof(NativeHelper))]
+namespace ExpressBase.Mobile.iOS.Helpers
+{
+    public class NativeHelper : INativeHelper
+    {
+        public void CloseApp()
+        {
+            Process.GetCurrentProcess().CloseMainWindow();
+            Process.GetCurrentProcess().Close();
+        }
+    }
+}
