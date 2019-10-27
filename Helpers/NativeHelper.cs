2a8aa07051d135f8a65a50d7871a130263f84fac
Amalkdev <amaldev@expressbase.com>
2019-10-27 20:47:15 +0530

    toast

--- Helpers/NativeHelper.cs
+++ Helpers/NativeHelper.cs
@@ -9,6 +9,7 @@ using Foundation;
 using UIKit;
 
 [assembly: Xamarin.Forms.Dependency(typeof(NativeHelper))]
+[assembly: Xamarin.Forms.Dependency(typeof(ToastMessage))]
 namespace ExpressBase.Mobile.iOS.Helpers
 {
     public class NativeHelper : INativeHelper
@@ -17,6 +18,41 @@ namespace ExpressBase.Mobile.iOS.Helpers
         {
             Process.GetCurrentProcess().CloseMainWindow();
             Process.GetCurrentProcess().Close();
+        }
+    }
+
+    public class ToastMessage : IToast
+    {
+        const double LONG_DELAY = 3.5;
+
+        NSTimer alertDelay;
+        UIAlertController alert;
+
+        public void Show(string message)
+        {
+            ShowAlert(message, LONG_DELAY);
+        }
+
+
+        void ShowAlert(string message, double seconds)
+        {
+            alertDelay = NSTimer.CreateScheduledTimer(seconds, (obj) =>
+            {
+                dismissMessage();
+            });
+            alert = UIAlertController.Create(null, message, UIAlertControllerStyle.Alert);
+            UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(alert, true, null);
+        }
+        void dismissMessage()
+        {
+            if (alert != null)
+            {
+                alert.DismissViewController(true, null);
+            }
+            if (alertDelay != null)
+            {
+                alertDelay.Dispose();
+            }
         }
     }
 }
