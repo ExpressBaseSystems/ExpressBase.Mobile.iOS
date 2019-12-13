622ce59cf701e4cc5c51440b4476f42765edbb75
Amal k dev <amaldev@expressbase.com>
2019-12-13 17:03:28 +0530

    local directory

--- Helpers/NativeHelper.cs
+++ Helpers/NativeHelper.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using ExpressBase.Mobile.Enums;
 using ExpressBase.Mobile.iOS.Helpers;
 using ExpressBase.Mobile.Models;
 using Foundation;
@@ -18,10 +21,61 @@ namespace ExpressBase.Mobile.iOS.Helpers
 
         public string DeviceId => UIDevice.CurrentDevice.IdentifierForVendor.AsString();
 
+        public string NativeRoot => Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
         public void CloseApp()
         {
             Process.GetCurrentProcess().CloseMainWindow();
             Process.GetCurrentProcess().Close();
+        }
+
+        public bool DirectoryOrFileExist(string DirectoryPath, SysContentType Type)
+        {
+            try
+            {
+                var pathToNewFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), DirectoryPath);
+                if (Type == SysContentType.File)
+                {
+                    return File.Exists(pathToNewFolder);
+                }
+                else if (Type == SysContentType.Directory)
+                {
+                    return Directory.Exists(pathToNewFolder);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return false;
+        }
+
+        public string CreateDirectoryOrFile(string DirectoryPath, SysContentType Type)
+        {
+            try
+            {
+                var pathToNewFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), DirectoryPath);
+                if (Type == SysContentType.Directory)
+                {
+                    Directory.CreateDirectory(pathToNewFolder);
+                }
+                else
+                {
+                    File.Create(pathToNewFolder);
+                }
+                return pathToNewFolder;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return null;
+        }
+
+        public byte[] GetPhoto(string url)
+        {
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), url);
+            return File.ReadAllBytes(path);
         }
     }
 
