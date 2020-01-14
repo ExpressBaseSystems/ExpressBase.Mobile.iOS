03111b337e020efe3d787065990ea19306f6ad29
Amal K Dev <amaldev@expressbase.com>
2020-01-14 18:50:31 +0530

    custom render for combo label

--- CustomRenderer/CustomRenderer.cs
+++ CustomRenderer/CustomRenderer.cs
@@ -16,6 +16,7 @@ using Xamarin.Forms.Platform.iOS;
 [assembly: ExportRenderer(typeof(CustomDatePicker), typeof(CustomDatePickerRenderer))]
 [assembly: ExportRenderer(typeof(CustomPicker), typeof(CustomSelectRenderer))]
 [assembly: ExportRenderer(typeof(CustomSearchBar), typeof(CustomSearchRenderer))]
+[assembly: ExportRenderer(typeof(ComboBoxLabel), typeof(ComboLabelRenderer))]
 namespace ExpressBase.Mobile.iOS.CustomRenderer
 {
     class TextBoxRenderer : EntryRenderer
@@ -61,6 +62,14 @@ namespace ExpressBase.Mobile.iOS.CustomRenderer
     public class CustomSearchRenderer : SearchBarRenderer
     {
         protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.SearchBar> e)
+        {
+            base.OnElementChanged(e);
+        }
+    }
+
+    public class ComboLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Label> e)
         {
             base.OnElementChanged(e);
         }
