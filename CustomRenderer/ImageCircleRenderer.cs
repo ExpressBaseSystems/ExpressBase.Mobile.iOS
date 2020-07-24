f01dd5e2dd4bed343562fc2f13ce64afbd3c0585
Amal K Dev <amaldev@expressbase.com>
2020-07-24 21:55:12 +0530

    circle image renderer

---
+++ CustomRenderer/ImageCircleRenderer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using ExpressBase.Mobile.CustomControls;
+using ExpressBase.Mobile.iOS.CustomRenderer;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ImageCircle), typeof(ImageCircleRenderer))]
+namespace ExpressBase.Mobile.iOS.CustomRenderer
+{
+    public class ImageCircleRenderer : ImageRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement != null || Element == null) return;
+            CreateCircle();
+        }
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (e.PropertyName == VisualElement.HeightProperty.PropertyName || e.PropertyName == VisualElement.WidthProperty.PropertyName)
+            {
+                CreateCircle();
+            }
+        }
+        private void CreateCircle()
+        {
+            try
+            {
+                double min = Math.Min(Element.Width, Element.Height);
+                Layer.CornerRadius = (float)(min / 2.0);
+                Layer.MasksToBounds = false;
+                Layer.BorderColor = Color.White.ToCGColor();
+                Layer.BorderWidth = 1;
+                ClipsToBounds = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unable to create circle image: " + ex);
+            }
+        }
+    }
+}
