b007beab161f3f607a50d537d9b08b246f9479cf
Amal K Dev <amaldev@expressbase.com>
2020-09-11 21:32:16 +0530

    script helper

---
+++ Helpers/Script/CSharpScriptHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExpressBase.Mobile.Helpers;
+using ExpressBase.Mobile.Helpers.Script;
+using ExpressBase.Mobile.iOS.Helpers.Script;
+using Foundation;
+using Mono.CSharp;
+using UIKit;
+
+[assembly: Xamarin.Forms.Dependency(typeof(CSharpScriptHelper))]
+
+namespace ExpressBase.Mobile.iOS.Helpers.Script
+{
+    public class CSharpScriptHelper : IScriptHelper
+    {
+        private Evaluator evaluator;
+
+        public CSharpScriptHelper()
+        {
+            InitializeEvaluator();
+        }
+
+        private void InitializeEvaluator()
+        {
+            var settings = new CompilerSettings()
+            {
+                StdLib = true
+            };
+
+            var reportPrinter = new ConsoleReportPrinter();
+            var ctx = new CompilerContext(settings, reportPrinter);
+
+            evaluator = new Evaluator(ctx);
+        }
+
+        public T Evaluate<T>(string statement, string expr)
+        {
+            try
+            {
+                bool runnable = evaluator.Run(statement);
+                if (runnable)
+                {
+                    evaluator.Evaluate(expr, out object result, out bool _set);
+                    return (T)result;
+                }
+            }
+            catch (Exception ex)
+            {
+                EbLog.Info("Error at CSharpScripting evaluate");
+                EbLog.Info(ex.Message);
+            }
+
+            return default;
+        }
+    }
+}
