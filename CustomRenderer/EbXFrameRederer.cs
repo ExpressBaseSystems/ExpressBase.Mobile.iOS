199e9af44f0ce1356bb0307ffff017edb129cac4
Amal K Dev <amaldev@expressbase.com>
2020-12-23 16:50:46 +0530

    renderer

---
+++ CustomRenderer/EbXFrameRederer.cs
@@ -0,0 +1,21 @@
+﻿using ExpressBase.Mobile.CustomControls;
+using ExpressBase.Mobile.iOS.CustomRenderer;
+using System.Drawing;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(EbXFrame), typeof(EbXFrameRederer))]
+
+namespace ExpressBase.Mobile.iOS.CustomRenderer
+{
+    public class EbXFrameRederer : FrameRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Frame> element)
+        {
+            base.OnElementChanged(element);
+
+            var elem = (EbXFrame)Element;
+        }
+    }
+}
